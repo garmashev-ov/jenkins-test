043a7077d28b207f3ad3889690b10c5d3cda51e6
garmashev-ov <99150121+garmashev-ov@users.noreply.github.com>
2024-11-24 22:26:07 +0300

    Update AppTest.java

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -2,10 +2,12 @@ package com.example;
 
 import static org.junit.Assert.assertEquals;
 
+import io.qameta.allure.Description;
 import org.junit.Test;
 
 public class AppTest {
   @Test
+  @Description("Тест для проверки сложения")
   public void testAdd() {
     App app = new App();
     assertEquals(5, app.add(2, 3));
@@ -13,6 +15,7 @@ public class AppTest {
   }
 
   @Test
+  @Description("Тест для вычитания")
   public void testSubtract() {
     App app = new App();
     assertEquals(-1, app.subtract(2, 3));
@@ -20,6 +23,7 @@ public class AppTest {
   }
 
   @Test
+  @Description("Тест умножения")
   public void testMultiply() {
     App app = new App();
     assertEquals(6, app.multiply(2, 3));
@@ -27,6 +31,7 @@ public class AppTest {
   }
 
   @Test
+  @Description("Тест для деления")
   public void testDivide() {
     App app = new App();
     assertEquals(3, app.divide(9, 3), 0.0001);
