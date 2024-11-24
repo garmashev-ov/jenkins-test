ea42541872c9b0fe7a25cd6d4e4a5d72895f87c2
garmashev-ov <99150121+garmashev-ov@users.noreply.github.com>
2024-11-24 23:17:41 +0300

    Update AppTest.java

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -1,40 +1,37 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
-
+import junit.framework.TestCase;
 import io.qameta.allure.Description;
-import org.junit.Test;
 
-public class AppTest {
-  @Test
-  @Description("Тест для проверки сложения")
-  public void testAdd() {
-    App app = new App();
-    assertEquals(5, app.add(2, 3));
-    assertEquals(-1, app.add(2, -3));
-  }
 
-  @Test
-  @Description("Тест для вычитания")
-  public void testSubtract() {
-    App app = new App();
-    assertEquals(-1, app.subtract(2, 3));
-    assertEquals(5, app.subtract(2, -3));
-  }
+public class AppTest extends TestCase {
 
-  @Test
-  @Description("Тест умножения")
-  public void testMultiply() {
-    App app = new App();
-    assertEquals(6, app.multiply(2, 3));
-    assertEquals(-6, app.multiply(2, -3));
-  }
+    @Description("Тест для проверки сложения")
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+        assertEquals(-1, app.add(2, -3));
+    }
 
-  @Test
-  @Description("Тест для деления")
-  public void testDivide() {
-    App app = new App();
-    assertEquals(3, app.divide(9, 3), 0.0001);
-    assertEquals(-3, app.divide(9, -3), 0.0001);
-  }
+    @Description("Тест для вычитания")
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(-1, app.subtract(2, 3));
+        assertEquals(5, app.subtract(2, -3));
+    }
+
+    @Description("Тест умножения")
+    public void testMultiply() {
+        App app = new App();
+        assertEquals(6, app.multiply(2, 3));
+        assertEquals(-6, app.multiply(2, -3));
+    }
+
+    @Description("Тест для деления")
+    public void testDivide() {
+        App app = new App();
+        assertEquals(3, app.divide(9, 3), 0.0001);
+        assertEquals(-3, app.divide(9, -3), 0.0001);
+    }
 }
